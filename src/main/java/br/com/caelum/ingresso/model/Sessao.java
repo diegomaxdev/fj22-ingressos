722d78d12b9956b0a9bcb2cd504f8d2e2d8fd3f8
Diego de Andrade Ferreira <diego.maxdev@gmail.com>
2019-10-05 16:26:39 -0300

    Criamos a classe Controller e a classe do Model para a Sessao

---
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -0,0 +1,59 @@
+package br.com.caelum.ingresso.model;
+
+import java.time.LocalTime;
+
+public class Sessao {
+	
+	private Integer id;
+	private LocalTime horario;
+	private Sala sala;
+	private Filme filme;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public LocalTime getHorario() {
+		return horario;
+	}
+	
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}
+	
+	public Sala getSala() {
+		return sala;
+	}
+	
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+	
+	public Filme getFilme() {
+		return filme;
+	}
+	
+	public void setFilme(Filme filme) {
+		this.filme = filme;
+	}
+	
+	public Sessao(Integer id, LocalTime horario, Sala sala, Filme filme) {
+		super();
+		this.id = id;
+		this.horario = horario;
+		this.sala = sala;
+		this.filme = filme;
+	}
+	
+	@Override
+	public String toString() {
+		return "Sessao [id=" + id + ", horario=" + horario + ", sala=" + sala + ", filme=" + filme + "]";
+	}
+
+	
+	
+}
